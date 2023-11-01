67f8b3dfbdeea9de1756d016d13db97209d3344e
gpw431 <gpw431@gmail.com>
2023-11-01 21:59:30 +0900

    project 생성

---
+++ src/main/java/kr/spring/EveryGame1Application.java
@@ -0,0 +1,13 @@
+package kr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryGame1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryGame1Application.class, args);
+	}
+
+}
